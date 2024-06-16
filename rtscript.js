90628c939d2d3597309b23da4e893cf6b101a025
Sgamb29 <shagamb@gmail.com>
2024-06-16 12:43:12 -0600

    modifications, bug fix, and updates

--- rtscript.js
+++ rtscript.js
@@ -1,6 +1,7 @@
 
 const startPageEl = document.getElementById("startPage");
 const totalPagesEL = document.getElementById("totalPages");
+const pagesTickNum = document.getElementById("pagesTickNum");
 const output = document.getElementById("output");
 
 const spaceBarChoice = document.getElementById("space");
@@ -8,15 +9,20 @@ const tapChoice = document.getElementById("tap");
 
 let detectMethod = "spacebar";
 
+const tapZone = document.getElementById("tapZone");
+
+
 spaceBarChoice.addEventListener("click", () => {
     if (spaceBarChoice.checked) {
         detectMethod = "spacebar";
+        tapZone.hidden = true;
     }
 });
 
 tapChoice.addEventListener("click", () => {
     if (tapChoice.checked) {
         detectMethod = "tap";
+        tapZone.hidden = false;
     }
 });
 
@@ -25,19 +31,26 @@ let totalPages = 0;
 let startSeconds = 0;
 let elapsedSeconds = 0;
 let pagesLeft = 0;
+let pagesPerTick = 0;
 
 let isTrackingStarted = false;
 
 const isNumeric = (string) => /^[+-]?\d+(\.\d+)?$/.test(string)
 
 function startTracking() {
-    if (isNumeric(startPageEl.value) && isNumeric(totalPagesEL.value) && !isTrackingStarted) {
+    if (isNumeric(startPageEl.value) && isNumeric(totalPagesEL.value) && isNumeric(pagesTickNum.value) && !isTrackingStarted) {
         currentPage = parseInt(startPageEl.value);
         totalPages = parseInt(totalPagesEL.value);
+        pagesPerTick = parseInt(pagesTickNum.value);
         pagesLeft = totalPages - currentPage;
         startSeconds = Date.now() / 1000;
-        output.innerText = "Tracking Started!";
+        output.innerText = "Tracking Started!\n\n";
         isTrackingStarted = true;
+        if (detectMethod == "tap") {
+            tapStartDelay();
+        }
+    } else if (!isTrackingStarted) {
+        output.innerText = "Start Page, Total Pages, and Pages Per Press Need To Be Filled Out!\n";
     }
 
     
@@ -50,7 +63,7 @@ function calculateStats() {
     let timeLeft = elapsedSeconds * pagesLeft / 60;
     let hoursLeft = parseInt(timeLeft / 60);
     let minutesLeft = timeLeft - hoursLeft * 60;
-    let pagesPerMin = (1 / (elapsedSeconds / 60)).toFixed(2);
+    let pagesPerMin = (pagesPerTick / (elapsedSeconds / 60)).toFixed(2);
     output.innerText = "Current Pages Per Minute: " + (pagesPerMin).toString() + " Time Left To Finish Book: " + hoursLeft + " Hours and " + parseInt(minutesLeft) + " Minutes";
     startSeconds = Date.now() / 1000;
 
@@ -63,8 +76,35 @@ document.addEventListener("keydown", (key) => {
     }
 });
 
-document.addEventListener("click", (ev) => {
-    if (startSeconds != 0 && detectMethod == "tap") {        
+let startDetectingTaps = false;
+
+function tapStartDelay() {
+    setTimeout(() => {
+        startDetectingTaps = true;
+    }, 250);
+}
+
+
+tapZone.addEventListener("click", (ev) => {
+    if (startSeconds != 0 && detectMethod == "tap" && startDetectingTaps) {
+        console.log(startSeconds);        
         calculateStats();
     }
 });
+
+
+function reloadPage() {
+    
+    currentPage = 0;
+    totalPages = 0;
+    startSeconds = 0;
+    elapsedSeconds = 0;
+    pagesLeft = 0;
+    pagesPerTick = 0;
+
+    isTrackingStarted = false;
+
+    output.innerText = "Tracking Stopped\n\n";
+    startDetectingTaps = false;
+
+}
